7062ed7cdfa6d7785821c07ebceae0fd424b44c4
esca2217 <estefanicarol.3b3@gmail.com>
2024-02-26 12:06:33 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Estefani Lopes da Silva to start working on your app!</Text>
+      <Text>Ana Carolina Paganini Olindino to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
